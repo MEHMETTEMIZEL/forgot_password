fa63ed4ae22f509b72d321e411d1359b61bde27d
Mehmet Temizel <mehmet.temizel@boun.edu.tr>
2020-05-23 13:13:10 +0300

    Initial django project

---
+++ rest_api/forgot_password/forgot_password.py
@@ -0,0 +1,4 @@
+from django.db import connection
+from platon_api.settings import DATABASES, USER_TABLENAME, WEBSITE_URL, JOB_LIST_API_URL
+from django.http import JsonResponse
+
---
+++ rest_api/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, HttpResponse
+#from rest_api.forgot_password import forgot_password
+
+#def forgot_password(request):
+ #     return forgot_password(request)
+
+
+
+# Create your views here.
